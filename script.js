5df2ff0ea8e699a578aea39ec9ab128305c86a6c
shilpipriya06 <139982930+shilpipriya06@users.noreply.github.com>
2024-08-14 00:35:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=bef4b6e4-0fbb-4e11-9410-6e16e2ca5ab1&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            
+            //add your api key from cricketdata.org
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
